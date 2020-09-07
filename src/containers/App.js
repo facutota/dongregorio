51ce9fde0d39c2dddb5a93b314a75779b7991a09
Facutota <facutota@yahoo.com.ar>
2020-09-07 14:23:47 -0300

    redux
    redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import {setSearchField } from '../actions';
 
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 	
 	constructor(){
 		super()
 		this.state = {
-			camiones: [],
-			searchfield: ''
+			camiones: []		
 		}
 	}
 
@@ -22,22 +34,22 @@ class App extends Component {
 			.then(users => this.setState({ camiones: users}));
 	}
 
-	onSearchChange = (event) => {
-		this.setState( {searchfield: event.target.value} )		
-	}
+	
 
 	render(){
-		const filteredCamiones = this.state.camiones.filter(camion => {
-			return camion.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { camiones } = this.state;
+		const { searchField, onSearchChange } = this.props;
+		const filteredCamiones = camiones.filter(camion => {
+			return camion.name.toLowerCase().includes(searchField.toLowerCase());
 		})
-		if(this.state.camiones.length === 0){
+		if(camiones.length === 0){
 			return <h1> Loading </h1>
 		}else{
 
 			return (
 					<div className = 'tc'>
 						<h1 className='f2' >Camiones Don Gregorio</h1>
-						<SearchBox searchChange={this.onSearchChange} />
+						<SearchBox searchChange={onSearchChange} />
 						<Scroll>
 							<CardList camiones={filteredCamiones} />
 						</Scroll>
@@ -50,4 +62,4 @@ class App extends Component {
 	}
 	
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import {searchCamiones} from './reducers';
 import 'tachyons';
+
+const logger = createLogger();
+const store = createStore(searchCamiones, applyMiddleware(logger))
 
 
 ReactDOM.render(
   <React.StrictMode>
-  	<App />
+  <Provider store={store}>
+  	<App  />
+  </Provider>	
   </React.StrictMode>,
   document.getElementById('root')
 );
